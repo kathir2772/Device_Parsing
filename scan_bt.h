201362886bde79a07947902b9c5e784ce6191cfc
Kathiravan <kathir.rahul@gmail.com>
2017-02-17 11:42:33 +0530

    scanning of Bluetooth added

---
+++ scan_bt.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include<string.h>
+void scan(void);
+
